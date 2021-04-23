036a829491f7c67ffcd2ef4ce2786f698e3b56ac
Alex <me@a9e.sh>
2021-04-22 23:59:47 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use yew::prelude::*;
+
+struct Model {
+
+}
+
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <>
+              <h1>{ "Hi" }</h1>
+            </>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<Model>();
+}
