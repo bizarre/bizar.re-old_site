316d07c0ff4a76257fbe9cbe7866b3d9ad610bd3
bizarre <me@a9e.sh>
2021-05-02 10:43:48 +0000

    Deploying to gh-pages from @ bizarre/bizarre@34a10a8de71f739d50b8856537fed01ee7411867 🚀

--- index-62fc7011c23ae14e.js
+++ index-62fc7011c23ae14e.js
@@ -269,7 +269,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-e569d590ac92a9df_bg.wasm', import.meta.url);
+        input = new URL('index-62fc7011c23ae14e_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
